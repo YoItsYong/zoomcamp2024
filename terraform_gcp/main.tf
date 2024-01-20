393f62b9e3f533e66ae5fdd54344f16df284d834
Yong <111252087+YoItsYong@users.noreply.github.com>
2024-01-20 20:44:57 +0000

    troubleshooting commits

---
+++ terraform_gcp/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.12.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "civil-charmer-410021"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "civil-charmer-410021-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
