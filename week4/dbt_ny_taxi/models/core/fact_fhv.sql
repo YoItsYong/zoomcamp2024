23ed0455524a131205af7a7c0e8dea183088cdbe
Yong <yongxlos@gmail.com>
2024-02-16 22:36:34 +0000

    adding fhv data

---
+++ week4/dbt_ny_taxi/models/core/fact_fhv.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with fhv_tripdata as (
+    select *,
+        'FHV' as service_type
+    from {{ ref('stg_fhv_taxi_data_2019') }}
+),
+dim_zones as (
+    select * from {{ ref('dim_zones') }}
+    where borough != 'Unknown'
+)
+select fhv_tripdata.tripid, 
+    fhv_tripdata.pickup_locationid, 
+    fhv_tripdata.dropoff_locationid,
+    fhv_tripdata.pickup_datetime, 
+    fhv_tripdata.dropoff_datetime, 
+from fhv_tripdata
+inner join dim_zones as pickup_zone
+on fhv_tripdata.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
+
